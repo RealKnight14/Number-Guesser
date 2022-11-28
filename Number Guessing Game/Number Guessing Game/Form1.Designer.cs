cc639ab0dc73d4152c942448e9477046d3c7f795
Knight <82474150+RealKnight14@users.noreply.github.com>
2022-11-28 09:43:02 +0000

    Add files via upload

---
+++ Number Guessing Game/Number Guessing Game/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Number_Guessing_Game
+{
+    partial class frmnumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtEnterNumber = new System.Windows.Forms.TextBox();
+            this.lblthink = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(317, 293);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(212, 71);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.CheckNumber);
+            // 
+            // txtEnterNumber
+            // 
+            this.txtEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnterNumber.Location = new System.Drawing.Point(237, 169);
+            this.txtEnterNumber.Name = "txtEnterNumber";
+            this.txtEnterNumber.Size = new System.Drawing.Size(358, 31);
+            this.txtEnterNumber.TabIndex = 1;
+            this.txtEnterNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblthink
+            // 
+            this.lblthink.AutoSize = true;
+            this.lblthink.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblthink.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblthink.Location = new System.Drawing.Point(212, 49);
+            this.lblthink.Name = "lblthink";
+            this.lblthink.Size = new System.Drawing.Size(393, 25);
+            this.lblthink.TabIndex = 2;
+            this.lblthink.Text = "I Am Thinking Of A Number Between";
+            this.lblthink.Click += new System.EventHandler(this.lblthink_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(34, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "You Guessed 0 Times";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmnumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblthink);
+            this.Controls.Add(this.txtEnterNumber);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "frmnumber";
+            this.Text = "Guess The Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+        private System.Windows.Forms.Label lblthink;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Number Guessing Game/Number Guessing Game/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+
+
+    public partial class frmnumber : Form
+    {
+
+        Random randomnumber = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+        public frmnumber()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void lblthink_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(txtEnterNumber.Text);
+
+            guesses += 1;
+
+            if (i == number)
+            {
+                MessageBox.Show("Nice, you guessed it. Try Another");
+                loadQuestions();
+                txtEnterNumber.Text = "";
+                guesses = 0;
+
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Go Higher");
+            }
+            else
+            {
+                MessageBox.Show("Go Lower");
+
+
+            }
+        }
+
+
+        private void loadQuestions()
+        {
+            number = randomnumber.Next(0, 10);
+
+            lblthink.Text = "I am thinking of a number between 0 and 10";
+        }
+            
+    }
+}
+
